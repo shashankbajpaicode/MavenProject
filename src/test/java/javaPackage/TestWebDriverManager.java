7ff0c128792aeec36b05c60ced69a935a4b31f0a
Shashank Bajpai <shashank@Shashanks-MacBook-Air.local>
2026-05-06 21:26:16 +0530

    Initial commit - Selenium project

---
+++ src/test/java/javaPackage/TestWebDriverManager.java
@@ -0,0 +1,30 @@
+package javaPackage;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestWebDriverManager {
+
+	@Test
+	public void a() {
+		WebDriverManager.chromedriver().setup();
+
+		WebDriver driver = new ChromeDriver();
+
+		driver.get("https://www.paytm.com");
+
+	}
+
+}
+
+/*
+
+What is WebDriverManager?
+
+WebDriverManager is a Java library that automates the management of browser drivers. 
+It downloads the correct driver version, sets the system path, and eliminates the need for manual driver setup.
+
+*/
